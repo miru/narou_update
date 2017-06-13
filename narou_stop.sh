a3b6b9d425ddecedaaab121e60cb890cc3721644
mirupreza <mirupreza@gmail.com>
2017-06-14 01:09:13 +0900

    modified:   narou_stop.sh

--- narou_stop.sh
+++ narou_stop.sh
@@ -4,7 +4,7 @@ cd ~/narou
 
 for NID in $*
 do
-    narou.rb tag -d "kindle 購読中 購読中な 未変換 未転送 変換済み 未読" $NID
+    narou.rb tag -d "kindle 購読中 購読中な 購読中R18 未変換 未転送 変換済み 未読" $NID
     narou.rb tag -a "切" $NID
 done
 
