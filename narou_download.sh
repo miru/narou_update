4ea733eaba315a2329739320aca000dcc51c6fd8
mirupreza <mirupreza@gmail.com>
2018-01-02 01:47:44 +0900

    modified:   narou_download.sh

--- narou_download.sh
+++ narou_download.sh
@@ -28,7 +28,7 @@ if [ -f "./download.txt" -a -s "./download.txt" ]; then
     URLS=`egrep -v "(^$|^#)" $DOWN_HISTORY/download.$DT.txt`
     $NAROU d -n $URLS
     $NAROU tag -a "未読" $URLS
-    $NAROU tag -a -c white "$YYYYMMDD" $URLS
+    $NAROU tag -a "$YYYYMMDD" -c white $URLS
 	$NAROU list -t "未読 既読" | $NAROU tag -d "未読"
 	$NAROU list -t "未読 切"   | $NAROU tag -d "未読"
 
